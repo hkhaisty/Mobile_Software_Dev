1ec075acee73dc81c93bbd6a991e65ba27261db0
Harry Haisty <h.k.haisty@gmail.com>
2019-01-16 15:41:13 -0500

    removes action bar

---
+++ InstantMessengerTesting/app/src/main/java/com/example/mdinh/instantmessengertesting/LoginActivity.kt
@@ -0,0 +1,27 @@
+package com.example.mdinh.instantmessengertesting
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        loginLogin_button.setOnClickListener{
+            val email = emailLogin_edittext.text.toString();
+            val password = passwordLogin_edittext.text.toString();
+
+            Log.d("LoginActivity", "Email: " + email);
+            Log.d("LoginActivity", "Password: + $password");
+        }
+
+        donthaveaccountLogin_textview.setOnClickListener{
+            Log.d("MainActivity", "Accessing registration screen for new users.")
+            finish();
+        }
+    }
+}
