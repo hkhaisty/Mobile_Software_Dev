0d344690ce81844128d2df00ff135a8be11ee8d7
hkhaisty <h.k.haisty@gmail.com>
2019-03-11 13:09:06 -0400

    Override

--- InstantMessengerTesting/app/src/main/java/com/example/mdinh/instantmessengertesting/QRHomeActivity.kt
+++ InstantMessengerTesting/app/src/main/java/com/example/mdinh/instantmessengertesting/QRHomeActivity.kt
@@ -4,14 +4,20 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 <<<<<<< HEAD
+<<<<<<< HEAD
 import kotlinx.android.synthetic.main.activity_qrhome.*
 import kotlinx.android.synthetic.main.loading_screen.view.*
 import android.widget.Button
 =======
+=======
+>>>>>>> 3ccaf4941388fb8c276d2f824421cb6514761da6
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.widget.Button
 import java.util.jar.Manifest
+<<<<<<< HEAD
+>>>>>>> 3ccaf4941388fb8c276d2f824421cb6514761da6
+=======
 >>>>>>> 3ccaf4941388fb8c276d2f824421cb6514761da6
 
 class QRHomeActivity : AppCompatActivity() {
@@ -20,6 +26,7 @@ class QRHomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qrhome)
 
+<<<<<<< HEAD
 <<<<<<< HEAD
         scanQRCode.imageView;
 
@@ -35,6 +42,8 @@ class QRHomeActivity : AppCompatActivity() {
                 startActivity(intent)
             }
 =======
+=======
+>>>>>>> 3ccaf4941388fb8c276d2f824421cb6514761da6
         val REQUEST_CODE = 100
         val PERMISSION_REQUEST = 200
 
@@ -43,6 +52,9 @@ class QRHomeActivity : AppCompatActivity() {
         scanBtn.setOnClickListener {
             val intent = Intent(this@QRHomeActivity, QRReaderActivity::class.java)
             startActivity(intent)
+<<<<<<< HEAD
+>>>>>>> 3ccaf4941388fb8c276d2f824421cb6514761da6
+=======
 >>>>>>> 3ccaf4941388fb8c276d2f824421cb6514761da6
         }
     }
