4d28f2bcc3e5b5dbaa42f96567bfc1a5bb2013af
hkhaisty <h.k.haisty@gmail.com>
2019-03-11 12:52:11 -0400

    Safety commit during cleanup

--- InstantMessengerTesting/app/src/main/java/com/example/mdinh/instantmessengertesting/QRHomeActivity.kt
+++ InstantMessengerTesting/app/src/main/java/com/example/mdinh/instantmessengertesting/QRHomeActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_qrhome.*
 import kotlinx.android.synthetic.main.loading_screen.view.*
+import android.widget.Button
 
 class QRHomeActivity : AppCompatActivity() {
 
@@ -14,9 +15,17 @@ class QRHomeActivity : AppCompatActivity() {
 
         scanQRCode.imageView;
 
-        scanQRCode.setOnClickListener{
+        scanQRCode.setOnClickListener {
             val intent = Intent(this@QRHomeActivity, ScanQRCodeActivity::class.java)
-            startActivity(intent)
+            val REQUEST_CODE = 100
+            val PERMISSION_REQUEST = 200
+
+            val scanBtn = findViewById<Button>(R.id.scanbtn)
+
+            scanBtn.setOnClickListener {
+                val intent = Intent(this@QRHomeActivity, QRReaderActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
